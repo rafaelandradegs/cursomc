d6ad1f0ede6b09f1674331b111bd9d5d14426f59
rafaelandradegs <rafaelandradegs@gmail.com>
2021-05-15 14:52:09 -0300

    Projeto criado

---
+++ src/test/java/com/rafael/projetocurso/ProjetocursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rafael.projetocurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetocursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
